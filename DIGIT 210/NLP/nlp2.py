b62ca3371f52a22aad07657f6609318b732fad66
wdjacca <chanjacqueline0418@gmail.com>
2021-04-15 01:35:49 -0400

    NLP Ex2

--- DIGIT 210/NLP/nlp2.py
+++ DIGIT 210/NLP/nlp2.py
@@ -1,8 +1,3 @@
-# Objective for Python Ex 2: explore lemmas and POS and named entities in your text blobs
-# Try counting the top 10 or 20 of a thing
-# Try a basic Python plotting library to create an SVG: We'll try Pygal
-# See https://towardsdatascience.com/interactive-data-visualization-in-python-with-pygal-4696fccc8c96
-
 from collections import Counter
 import pygal
 import spacy
@@ -13,71 +8,69 @@ lemmatizer = nlp.get_pipe("lemmatizer")
 print(lemmatizer.mode)  # 'rule'
 
 disneysongs = open('disneySongLyrics.txt', 'r')
-# If, when you print the file, it comes out with weird characters in place of apostrophes, quotes, etc.,
-# try adapting this alternative version of the line above:
-# yourtext = open('yourtext.txt', 'r', encoding="utf8", errors='ignore')
 words = disneysongs.read()
 disneywords = nlp(words)
-# print(words)
 
 
-def verbcollector(words):
-    Verbs = []
+
+def punctuation(words):
+    Punct = []
     count = 0
     for token in words:
-        if token.pos_ == "VERB":
+        if token.pos_ == "PUNCT":
             count += 1
-            # print(count, ": ", token.text, " lemma: ", token.lemma_, " pos: ", token.pos_)
-            # don't forget the underscore after token.lemma_ , token.pos_, etc.!
-            Verbs.append(token.lemma_)
+            Punct.append(token.lemma_)
             print(count, ": ", token, token.pos_, spacy.explain(token.pos_))
-    # print(count, ": ", Verbs)
-    return Verbs
+    return Punct
 
-listVerbs = verbcollector(disneywords)
-verb_freq = Counter(listVerbs)
-topTen = verb_freq.most_common(10)
-print(topTen)
-lastTen = verb_freq.most_common()[:-5:-1]
-# print(lastTen)
-
-# ebb: Okay, let's try out the pygal graphing library!
-# Here I am experimenting and creating TWO bar graphs. For homework you only need to create one.
-# I made one bar graph called bar_chartOver10, and another that I like much better called bar_chartTopTen
-bar_chartOver10 = pygal.Bar()
-bar_chartTopTen = pygal.Bar()
-
-bar_chartOver10.title = 'Verbs Used Over 10 Times in Disney Songs'
-bar_chartTopTen.title='Top 10 Verbs in Disney Songs'
-
-for v in verb_freq:
-    # verb_freq is a dictionary structure, so we return its key and its value:
-    print(v, verb_freq[v])
-    if verb_freq[v] > 10:
-        bar_chartOver10.add(v, verb_freq[v])
+listPunct = punctuation(disneywords)
+punct_count = Counter(listPunct)
+topThree = punct_count.most_common(3)
+print(topThree)
 
 
-for t in topTen:
-    # this is a list of tuples, so we return its values like this:
+bar_chartTopThree = pygal.Bar()
+
+bar_chartTopThree.title = 'Top Three Punctuation used in Disney Songs'
+
+for t in topThree:
     print(t[0], t[1])
-    bar_chartTopTen.add(t[0], t[1])
+    bar_chartTopThree.add(t[0], t[1])
 
 # print(bar_chart)
-print(bar_chartOver10.render(is_unicode=True))
-bar_chartOver10.render_to_file('bar_chartOver10.svg')
-bar_chartTopTen.render_to_file('bar_chartTopTen.svg')
+print(bar_chartTopThree.render_to_file('bar_chartTopThree.svg'))
+
+def nounfinder(words):
+    noun = []
+    count = 0
+    for token in words:
+        if token.pos_ == "NOUN":
+            count += 1
+            noun.append(token.lemma_)
+            print(count, ": ", token, token.pos_, spacy.explain(token.pos_))
+    return noun
+
+nounlist = nounfinder(disneywords)
+nouncount = Counter(nounlist)
+topFive = nouncount.most_common(5)
+print(topFive)
 
 
-# On windows ctrl / comments out blocks.
-# On mac command / comments out blocks
+bar_chartTopFive = pygal.Bar()
+bar_chartOver10 = pygal.Bar()
 
-# lowercaseList = []
-# for l in listVerbs:
-#     l = str(l).lower()
-#     lowercaseList.append(l)
-# setVerbs = set(lowercaseList)
-# count = 0
-# for s in setVerbs:
-#     count += 1
-# print(sorted(setVerbs))
-# print(count)
+bar_chartTopFive.title = 'Top Five Nouns used in Disney Songs'
+bar_chartOver10.title = "Nouns that appeared over 10 times in Disney Songs"
+
+for t in topFive:
+    print(t[0], t[1])
+    bar_chartTopFive.add(t[0], t[1])
+
+for n in nouncount:
+    print(n, nouncount[n])
+    if nouncount[n] > 10:
+        bar_chartOver10.add(n, nouncount[n])
+
+# print(bar_chart)
+print(bar_chartOver10.render(is_unicode=True), bar_chartTopFive.render_to_file('bar_chartTopFive.svg'),
+bar_chartOver10.render_to_file('bar_chartOver10.svg'))
